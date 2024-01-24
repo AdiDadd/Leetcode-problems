5b1085853a9c458d48ca85b1c4afd6ad651e5c40
Adithya <134584545+AdiDadd@users.noreply.github.com>
2024-01-24 09:48:54 +0530

    Time: 4 ms (50.14%), Space: 5.6 MB (99.20%) - LeetHub

--- 0027-remove-element/0027-remove-element.c
+++ 0027-remove-element/0027-remove-element.c
@@ -1,43 +1,34 @@
 int removeElement(int* nums, int numsSize, int val) {
-    int k = 0;  // Variable to store the count of elements not equal to val
-    
-    // Iterate through the array
+    int k = 0;  
     for (int i = 0; i < numsSize; i++) {
-        // If the current element is not equal to val
         if (nums[i] != val) {
-            // Move the non-equal element to the beginning of the array
             nums[k++] = nums[i];
         }
-    }
-    
-    return k;  // Return the count of elements not equal to val
+    }    
+    return k;
 }
 
 int Main() {
-    // Example 1
+
     int nums1[] = {3, 2, 2, 3};
     int val1 = 3;
-    int expectedNums1[] = {2, 2};
+   // int expectedNums1[] = {2, 2};
     int k1 = removeElement(nums1, 4, val1);
-
-    // Example 2
+    
     int nums2[] = {0, 1, 2, 2, 3, 0, 4, 2};
     int val2 = 2;
-    int expectedNums2[] = {0, 1, 4, 0, 3};
+   // int expectedNums2[] = {0, 1, 4, 0, 3};
     int k2 = removeElement(nums2, 8, val2);
 
-    // Printing the results
-    printf("Example 1: k = %d\n", k1);
+    printf("%d\n", k1);
     for (int i = 0; i < k1; i++) {
-        printf("%d ", nums1[i]);
+        printf("%d\n ", nums1[i]);
     }
-    printf("\n");
 
-    printf("Example 2: k = %d\n", k2);
+    printf("%d\n", k2);
     for (int i = 0; i < k2; i++) {
-        printf("%d ", nums2[i]);
+        printf("%d\n ", nums2[i]);
     }
-    printf("\n");
 
     return 0;
 }
